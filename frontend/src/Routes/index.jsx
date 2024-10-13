c6dd09edd2ea9fbfc8c7b5f2811802f6970e569b
Nhanntps3309 <vanbinhchathanhnhan@gmail.com>
2024-10-13 10:13:44 +0700

    Initial commit

--- frontend/src/Routes/index.jsx
+++ frontend/src/Routes/index.jsx
@@ -1,13 +1,16 @@
 import HeaderOnly from "@/layouts/HeaderOnly";
 import Home from "@/pages/home";
+import ListPage from "@/pages/list";
 import LoginPage from "@/pages/login/loginPage";
 import RegistrationForm from "@/pages/register/index";
+
 
 // Public Route
 const publicRoutes = [
     {path: "/" , component: Home},
     {path: "/login", component: LoginPage, layout: HeaderOnly},
-    {path: "/register", component: RegistrationForm, layout: HeaderOnly}
+    {path: "/register", component: RegistrationForm, layout: HeaderOnly},
+    {path: "/list", component: ListPage},
 ];
 
 const privateRoutes = []
